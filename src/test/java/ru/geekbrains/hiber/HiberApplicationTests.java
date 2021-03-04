7b7743cf51efe5c7cab9f9b56a103a31331a0c4c
Vladimir Neizhmakov <vovka_x@mail.ru>
2021-03-04 17:44:35 +0300

    Master

---
+++ src/test/java/ru/geekbrains/hiber/HiberApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.hiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
